755c2df58718a2baf9b9ec12d24564d41e52e4d7
Bishoy Sedra <bishosedra0@gmail.com>
2025-06-15 21:27:19 +0300

    --

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/react-app/',
 });
-// This configuration sets the base path for the application to '/Mini_InstaPay_Frontend/'
