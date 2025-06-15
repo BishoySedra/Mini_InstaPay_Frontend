6a0933fcba9d5a33efd8ba8d52ae810cc6d3db27
Bishoy Sedra <bishosedra0@gmail.com>
2025-06-15 21:34:04 +0300

    modified the base url for deploy!

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/vite-deploy-demo/',
+  base: '/Mini_InstaPay_Frontend/',
+  plugins: [react()]
 });
+// This configuration sets the base path for the application to '/Mini_InstaPay_Frontend/'
